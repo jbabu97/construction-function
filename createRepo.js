f4733d50d78241fd1c9efb63aeb5599ae7885e66
jbubu97 <jabbar.babu81@gmail.com>
2023-10-30 21:10:56 +0600

    first commit

---
+++ createRepo.js
@@ -0,0 +1,41 @@
+
+// userOne.createRepo('Learn Python', 'Muktadir Nayeem');
+// userOne.createRepo('Learn TyperScript', 'Jillur Rahman');
+// userOne.createRepo('Learn JavaScript', 'Jabbar Babu');
+
+const element =document.querySelector('.element');
+const heading = document.createElement('h1');
+heading.setAttribute('class', 'heading');
+heading.innerText = 'Practicing Construction Function';
+element.appendChild(heading)
+// console.log(heading);
+const repoDiv = document.createElement('div');
+element.appendChild(repoDiv);
+// console.log(element);
+
+
+const allRepo = userOne.getRepos();
+// console.log(allRepo);
+// const singleRepo = allRepo[0];
+// console.log(singleRepo);
+
+// const repoName = document.createElement('h2');
+// const creator = document.createElement('p');
+// const createAt = document.createElement('p');
+
+// repoName.innerText = `Repo Name: ${singleRepo.name}`;
+// element.appendChild(repoName);
+// creator.innerText = `Repo Name: ${singleRepo.creator}`;
+// element.appendChild(creator);
+// createAt.innerText = `Repo Name: ${singleRepo.createAt}`;
+// element.appendChild(createAt);
+
+allRepo.map(r => {
+    // console.log(r.name);
+    repoDiv.innerHTML = 
+    `
+    <h2>${r.name}</h2>
+    <p>${r.creator}</p>
+    <p>${r.createAt}</p>
+    `
+})
---
+++ script.js
@@ -0,0 +1,60 @@
+function UserAccount (name, email, userName){
+    this.name = name;
+    this.email = email;
+    this.userName = userName;
+    this.repos = [];
+
+    this.updateUser = function (fullName, email){
+        if (fullName !== undefined) {
+            this.name = fullName;
+        };
+        if (email !== undefined) {
+            this.email = email;
+        };
+    };
+
+    this.getRepos = function(){
+        return this.repos;
+    };
+
+    this.createRepo = function(repoName, repoCreator){
+        const date = new Date()
+        const repo = {
+            name : repoName,
+            creator: repoCreator,
+            createAt: date.toLocaleDateString()
+            // createAt: Date.now()
+        };
+        this.repos.push(repo);
+    };
+
+    this.updateRepo = function (repoName, updatedInfo){
+        const repoIndex = this.repos.findIndex(repo => repo.name === repoName);
+
+        if (repoIndex !== -1) {
+            const repoToUpdate = this.repos[repoIndex];
+            Object.assign(repoToUpdate, updatedInfo)
+            console.log(repoToUpdate, updatedInfo)
+        } else {
+            console.log(`Repository ${repoName} is not found.`);
+        };
+    }
+
+};
+
+
+const userOne = new UserAccount('Jabbar', 'Babu@gmail.com', 'jbabu97');
+
+// Create some repositories for the user
+userOne.createRepo('Learn JavaScript', 'jbabu97');
+userOne.createRepo('Learn Python', 'Muktadir Nayeem');
+
+// userOne.updateUser('Babu', 'jb@yahoo.com')
+
+// const newRepo = userOne.createRepo('jsLearning', 'Nayeem')
+
+const repoUpdate = userOne.updateRepo('Learn Python', {creator: 'Jillur Rahman', createAt: Date.now()});
+
+console.log(repoUpdate);
+console.log(userOne.getRepos());
+
